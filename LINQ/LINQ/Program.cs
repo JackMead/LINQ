009551d191b46e833874ab4cc9434abb38609121
JackMead <Jack.Mead@softwire.com>
2017-11-03 12:22:58 +0000

    Tested using ValueTuples instead of Tuples to improve readability

--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LINQ
 {
@@ -22,14 +20,15 @@ namespace LINQ
                 apples.Where(i => i.Poisoned).GroupBy(i => i.Colour).OrderByDescending(i => i.Count()).Skip(1).Take(1).ToList()[0].Key;
             Console.WriteLine("The second most commonly poisoned colour is {0}", secondLargestColourPoisonedApples);
 
-            var maxNumberNonPoisonedRedApplesSuccessive = apples.Aggregate(new Tuple<int, int>(0, 0), (acc, apple) =>
-                (apple.Colour == "Red" && !apple.Poisoned) 
-                ? new Tuple<int, int>(acc.Item1 + 1, Math.Max(acc.Item1 + 1, acc.Item2)) 
-                : new Tuple<int, int>(0, Math.Max(acc.Item1, acc.Item2))).Item2;
+            //Readjusted to use value tuples instead of tuple
+            var maxNumberNonPoisonedRedApplesSuccessive = apples.Aggregate( (0, 0), (acc, apple) =>
+                  (apple.Colour == "Red" && !apple.Poisoned)
+                  ?  (acc.Item1 + 1, Math.Max(acc.Item1 + 1, acc.Item2))
+                  :  (0, Math.Max(acc.Item1, acc.Item2))).Item2;
             Console.WriteLine("There are at most {0} red, non poisoned apples in a row", maxNumberNonPoisonedRedApplesSuccessive);
 
             var counter = 0;
-            var question3UsingZip = apples.Zip(apples.Skip(1), (a1, a2) => (a1.Colour=="Red" && !a1.Poisoned && a2.Colour=="Red" && !a2.Poisoned)?(counter==0?counter=2:++counter):counter=0).Max();
+            var question3UsingZip = apples.Zip(apples.Skip(1), (a1, a2) => (a1.Colour == "Red" && !a1.Poisoned && a2.Colour == "Red" && !a2.Poisoned) ? (counter == 0 ? counter = 2 : ++counter) : counter = 0).Max();
             Console.WriteLine("There are {0} non poisoned red in a row according to zip", question3UsingZip);
 
             var numberOfGreenApplesFollowedByGreenApples = apples.Aggregate(
@@ -40,7 +39,7 @@ namespace LINQ
 
             Console.WriteLine("There are {0} Green apples that are followed by green apples", numberOfGreenApplesFollowedByGreenApples);
 
-            var question4UsingZip = apples.Zip(apples.Skip(1), (a1, a2) => a1.Colour == "Green" && a2.Colour == "Green"  ).Count(i=>i);
+            var question4UsingZip = apples.Zip(apples.Skip(1), (a1, a2) => a1.Colour == "Green" && a2.Colour == "Green").Count(i => i);
             Console.WriteLine("There are {0} Green apples that are followed by green apples according to zip", question4UsingZip);
 
         }
